7a06627af246a6590f643ba9a997633f9658a139
juanitodread <juanitodread@gmail.com>
2017-06-23 16:28:40 -0500

    Combining reducers

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -1,22 +1,31 @@
-const reducer = (state, action) => {
+const recipesReducer = (recipes = [], action) => {
   switch(action.type) {
     case 'ADD_RECIPE':
-      return Object.assign({}, state, {
-        recipes: state.recipes.concat({name: action.name})
-      });
+      return recipes.concat({name: action.name})
+  }
 
+  return recipes;
+};
+
+const ingredientsReducer = (ingredients = [], action) => {
+  switch(action.type) {
     case 'ADD_INGREDIENT':
       const newIngredient = {
         name: action.name,
         recipe: action.recipe,
         quantity: action.quantity
       };
-      return Object.assign({}, state, {
-        ingredients: state.ingredients.concat(newIngredient)
-      });
+      return ingredients.concat(newIngredient)
   }
 
-  return state;
+  return ingredients;
+};
+
+const reducer = (state, action) => {
+  return Object.assign({}, state, {
+    recipes: recipesReducer(state.recipes, action),
+    ingredients: ingredientsReducer(state.ingredients, action)
+  });
 };
 
 module.exports = reducer;
