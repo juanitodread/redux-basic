d4c7a2079528b2f97198d0e6efd0847757b3d1f5
juanitodread <juanitodread@gmail.com>
2017-06-23 16:43:29 -0500

    Refactor action types to use constants

--- app/js/actions/ingredients.js
+++ app/js/actions/ingredients.js
@@ -1,5 +1,7 @@
+import {ADD_INGREDIENT} from '../constants/action-types';
+
 const addIngredient = (recipe, name, quantity) => ({
-  type: 'ADD_INGREDIENT', recipe, name, quantity
+  type: ADD_INGREDIENT, recipe, name, quantity
 });
 
 module.exports = addIngredient;
--- app/js/actions/recipes.js
+++ app/js/actions/recipes.js
@@ -1,5 +1,7 @@
+import {ADD_RECIPE} from '../constants/action-types';
+
 const addRecipe = (name) => ({
-  type: 'ADD_RECIPE', name
+  type: ADD_RECIPE, name
 });
 
 module.exports = addRecipe;
