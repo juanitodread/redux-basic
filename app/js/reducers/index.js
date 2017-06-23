98dbb9cc8e7b17812bf1a2e2ea71063d55ad989f
juanitodread <juanitodread@gmail.com>
2017-06-23 16:33:55 -0500

    Refactor reducers into different files

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -1,25 +1,5 @@
-const recipesReducer = (recipes = [], action) => {
-  switch(action.type) {
-    case 'ADD_RECIPE':
-      return recipes.concat({name: action.name})
-  }
-
-  return recipes;
-};
-
-const ingredientsReducer = (ingredients = [], action) => {
-  switch(action.type) {
-    case 'ADD_INGREDIENT':
-      const newIngredient = {
-        name: action.name,
-        recipe: action.recipe,
-        quantity: action.quantity
-      };
-      return ingredients.concat(newIngredient)
-  }
-
-  return ingredients;
-};
+import recipesReducer from './recipes';
+import ingredientsReducer from './ingredients';
 
 const reducer = (state, action) => {
   return Object.assign({}, state, {
